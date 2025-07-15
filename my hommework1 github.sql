2c6fcaf724c35a61d2f3877349a15c3324fd8acf
Saidamin Saidakhmadov <saidaxmadovsaidamin@gmail.com>
2025-07-15 18:26:28 +0500

    My first homework

---
+++ my hommework1 github.sql
@@ -0,0 +1,46 @@
+---Homework
+--Easy
+---1. 
+---2. Server
+---Database
+---Schema
+----Table
+---- rows and columns
+---3.The first one is with Windows Authentication
+----- and the second one with SQL Server Authentication
+
+
+--Medium
+
+create table Students (StudentsID int, StudentName varchar(50),Age int,)
+select *from Students
+insert into Students values (1,'Saidamin',16)
+insert into Students values (2,'Tolibjon',16)
+insert into Students values (3,'Soliha',16)
+insert into Students values (4,'Zahro',16)
+insert into Students values (5,'Muhamadsolih',16)
+
+--- SQL is a Language which can help to communicate with databases
+----SQL Server means Database Software (RDBMS) The main purpose of this is to store, manage and process data
+-----SSMS stands for SQL Server Management Store A graphical user interface (GUI) tool developed by Microsoft for managing SQL Server.The main idea of this it works with SQL Server
+
+
+
+----Hard
+--DQL Stands for Data Query Language.Main Purpose just reading  Data.Example:SELECT
+---DML Stands for Data Manipulation Language.Purpose of this Add/change or delete Data.Example:INSERT,UPDATE,DELETE
+----DDL Stands for Data Definition Language.Purpose Create or Chnage Structure.Example:CREATE,ALTER,TRUNCATE,DROP 
+-----DCL Stands for Data Control Language.Purpose To control user access.Example:GRANT,REVOKE
+------TCL Stands for Transaction Control Language.Main Purpose Control transactions(grouped steps).Example:BEGIN,COMMIT,ROLLBACK
+
+
+
+
+
+
+
+
+
+
+
+
